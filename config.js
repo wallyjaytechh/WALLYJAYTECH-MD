07fcb953e8757df7accfabf4b3b8ccf9f350877c
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2024-08-12 00:15:09 +0000

    ✌𝓦𝓐𝓛𝓛𝓨𝓙𝓐𝓨𝓣𝓔𝓒𝓗-𝓜𝓓✌

--- config.js
+++ config.js
@@ -1,130 +1,83 @@
-const fs = require('fs-extra')
-if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
-
-
-//═══════[Required Variables]════════\\
-global.audio= "" ;  
-global.video= "" ;
-global.port =process.env.PORT
-global.appUrl=process.env.APP_URL || ""                       // put your app url here,
-global.email ="wallyjay123@gmail.com"
-global.location="Osun,Nigeria."
-
-
-global.mongodb= process.env.MONGODB_URI || ""
-global.allowJids= process.env.ALLOW_JID || "null" 
-global.blockJids= process.env.BLOCK_JID || "null"
-global.DATABASE_URL = process.env.DATABASE_URL || ""
-
-global.timezone= process.env.TZ || process.env.TIME_ZONE || "Afriaca/Lagos";
-global.github=process.env.GITHUB|| "https://github.com/wallyjaytechh/WALLYJAYTECH-MD";
-global.gurl  =process.env.GURL  || "https://wa.me/2348144317152";
-global.website=process.env.GURL || "https://chat.whatsapp.com/FTuAbXCgwPLDt96BK7bm5P" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://graph.org/file/21e984a94def2215b0215.jpg" ; // SET LOGO FOR IMAGE 
-global.caption = process.env.CAPTION || global.caption || "ℙ𝕆𝕎𝔼ℝ𝔼𝔻 𝔹𝕐 𝕎𝔸𝕃𝕃𝕐 𝕁𝔸𝕐 𝕋𝔼ℂℍ" 
-
-
-global.devs = "2348144317152" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "2348054984935,2348106206532";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2348144317152";
-
-
-
-
-//========================= [ BOT SETTINGS ] ======8===================\\
-global.style = process.env.STYLE   || Math.floor(Math.random()*6) || '0'  // put '1' to "5" here to check bot styles
-global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
-global.gdbye = process.env.GOODBYE || process.env.CAN_GOODBYE || "true";  // Make it "false" to disable GOODBYE MESSAGE 
-global.wlcm  = process.env.WELCOME || process.env.CAN_WELCOME || "true";  // Make it "false" to disable WELCOME MESSAGE
-
-global.warncount = process.env.WARN_COUNT || 5
-global.disablepm = process.env.DISABLE_PM || "false"
-global.disablegroup = process.env.DISABLE_GROUPS || "false", // disable bot in groups when public mode
-
-global.MsgsInLog = process.env.MSGS_IN_LOG|| "false" // "true"  to see messages , "log" to show logs , "false" to hide logs messages
-global.userImages= process.env.USER_IMAGES || "text" // "https://graph.org/file/21e984a94def2215b0215.jpg" // set Image/video urls here
-global.waPresence= process.env.WAPRESENCE ||  "recording" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
-
-
-//========================= [ AUTO READ MSGS & CMDS ] =========================\\
-global.readcmds = process.env.READ_COMMAND || "false"
-global.readmessage = process.env.READ_MESSAGE || "true"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,234xxxxxxxxxx";
-
-
-//========================= [ AUTO SAVE & READ STATUS ] =========================\\
-global.read_status = process.env.AUTO_READ_STATUS || "true"
-global.save_status = process.env.AUTO_SAVE_STATUS || "true"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,234xxxxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "2348144317152,234xxxxxxxxxx";
-
-global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
-global.scan = "https://suhail-md-vtsf.onrender.com";
-
-
-global.SESSION_ID = process.env.SESSION_ID ||  ""  // PUT your SESSION_ID 
-
-
+const toBool = (x) => x == 'true'
+const { Sequelize } = require('sequelize')
+const { existsSync } = require('fs')
+const path = require('path')
+const configPath = path.join(__dirname, './config.env')
+const databasePath = path.join(__dirname, './database.db')
+if (existsSync(configPath)) require('dotenv').config({ path: configPath })
+const DATABASE_URL =
+  process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
-
-  menu: process.env.MENU || "A17_Md", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: WALLYJAYTECH-MD Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
-
-  HANDLERS: process.env.PREFIX  || ".",
-  BRANCH  : process.env.BRANCH  || "main",
-  VERSION : process.env.VERSION || "1.0",
-  caption : global.caption || "" , // ```『 ℙ𝕆𝕎𝔼ℝ𝔼𝔻 𝔹𝕐 𝕎𝔸𝕃𝕃𝕐 𝕁𝔸𝕐 𝕋𝔼ℂℍ 』```", //*『 SUBSCRIBE • WALLY JAY TECH 』*\n youtube.com/@wallyjaytechy"),
- 
-  author : process.env.PACK_AUTHER|| "Wally Jay Tech",
-  packname: process.env.PACK_NAME || "WALLYJAYTECH-MD",
-  botname : process.env.BOT_NAME  || "WALLYJAYTECH-MD",
-  ownername:process.env.OWNER_NAME|| "Wally Jay Tech",
-
-
-  errorChat : process.env.ERROR_CHAT || "",
-  KOYEB_API : process.env.KOYEB_API  || "false",
-
-  REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
-  antilink_values:process.env.ANTILINK_VALUES|| "all",
-  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
-
-
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "WALLYJAYTECH"  ).toUpperCase(),
-
-
-
-};
-
-
-
-global.ELEVENLAB_API_KEY = process.env.ELEVENLAB_API_KEY || "";
-global.aitts_Voice_Id = process.env.AITTS_ID|| "37";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-global.rank = "updated"
-global.isMongodb = false; 
-let file = require.resolve(__filename)
-fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename}'`);delete require.cache[file];	require(file); })
- 
+  VERSION: require('./package.json').version,
+  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  DATABASE:
+    DATABASE_URL === databasePath
+      ? new Sequelize({
+          dialect: 'sqlite',
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: 'postgres',
+          ssl: true,
+          protocol: 'postgres',
+          dialectOptions: {
+            native: true,
+            ssl: { require: true, rejectUnauthorized: false },
+          },
+          logging: false,
+        }),
+  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
+  SUDO: process.env.SUDO || '',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+  BRANCH: 'master',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,WALLYJAYTECH-MD',
+  ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
+  LOG_MSG: toBool(process.env.LOG_MSG) || false,
+  RMBG_KEY: process.env.RMBG_KEY || 'null',
+  BAILEYS_LOG_LVL: process.env.BAILEYS_LOG_LVL || 'silent',
+  LANG: (process.env.LANGUAG || 'en').toLowerCase(),
+  WARN_LIMIT: process.env.WARN_LIMIT || 3,
+  FORCE_LOGOUT: toBool(process.env.FORCE_LOGOUT),
+  BRAINSHOP: process.env.BRAINSHOP || '159501,6pq8dPiYt7PdqHz3',
+  DIS_BOT: process.env.DISABLE_BOT || 'null',
+  ANTILINK_MSG: process.env.ANTILINK_MSG || '_Antilink Detected &mention kicked_',
+  ANTISPAM_MSG: process.env.ANTISPAM_MSG || '_Antispam Detected &mention kicked_',
+  ANTIWORDS_MSG: process.env.ANTIWORDS_MSG || '_AntiWord Detected &mention kicked_',
+  ANTIWORDS: process.env.ANTIWORDS || 'word',
+  MENTION: process.env.MENTION || '',
+  MAX_UPLOAD: process.env.MAX_UPLOAD || 230,
+  REJECT_CALL: toBool(process.env.REJECT_CALL),
+  VPS: toBool(process.env.VPS),
+  AUTO_STATUS_VIEW: (process.env.AUTO_STATUS_VIEW || 'true').trim(),
+  SEND_READ: toBool(process.env.SEND_READ),
+  KOYEB: toBool(process.env.KOYEB),
+  KOYEB_NAME: (process.env.KOYEB_NAME || '').trim(),
+  KOYEB_API: (process.env.KOYEB_API || '').trim(),
+  AJOIN: toBool(process.env.AJOIN),
+  GPT: (process.env.GPT || 'free').trim(),
+  MODEL: (process.env.MODEL || 'gpt-3.5-turbo').trim(),
+  APPROVE: (process.env.APPROVE || '').trim(),
+  ANTI_DELETE: (process.env.ANTI_DELETE || 'null').trim(),
+  PERSONAL_MESSAGE: process.env.PERSONAL_MESSAGE || 'null',
+  DISABLE_START_MESSAGE: process.env.DISABLE_START_MESSAGE
+    ? toBool(process.env.DISABLE_START_MESSAGE)
+    : false,
+  ANTI_BOT: (process.env.ANTI_BOT || 'off').trim(),
+  ANTI_BOT_MESSAGE: process.env.ANTI_BOT_MESSAGE || '&mention removed',
+  WARN_MESSAGE:
+    process.env.WARN_MESSAGE ||
+    '⚠️WARNING⚠️\n*User :* &mention\n*Warn :* &warn\n*Remaining :* &remaining',
+  WARN_RESET_MESSAGE:
+    process.env.WARN_RESET_MESSAGE || `WARN RESET\nUser : &mention\nRemaining : &remaining`,
+  WARN_KICK_MESSAGE: process.env.WARN_KICK_MESSAGE || '&mention kicked',
+  TRUECALLER: process.env.TRUECALLER,
+  DELETE_TYPE: (process.env.DELETE_TYPE || '').trim(),
+  LIST_TYPE: (process.env.LIST_TYPE || 'text').trim(),
+  BING_COOKIE: (process.env.BING_COOKIE || '').trim(),
+  GEMINI_API_KEY: (process.env.GEMINI_API_KEY || '').trim(),
+  ADMINS: process.env.GROUP_ADMINS || '',
+  RENDER_NAME: (process.env.RENDER_NAME || '').trim(),
+  RENDER_API_KEY: (process.env.RENDER_API_KEY || '').trim(),
+}
