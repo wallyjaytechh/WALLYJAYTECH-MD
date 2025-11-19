ef19e609701c2e4279227d2effaa6e5cdaf8ebdf
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-18 19:13:34 -0800

    ✌𝓦𝓐𝓛𝓛𝓨𝓙𝓐𝓨𝓣𝓔𝓒𝓗-𝓜𝓓✌

---
+++ config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+global.APIs = {
+    xteam: 'https://api.xteam.xyz',
+    dzx: 'https://api.dhamzxploit.my.id',
+    lol: 'https://api.lolhuman.xyz',
+    violetics: 'https://violetics.pw',
+    neoxr: 'https://api.neoxr.my.id',
+    zenzapis: 'https://zenzapis.xyz',
+    akuari: 'https://api.akuari.my.id',
+    akuari2: 'https://apimu.my.id',
+    nrtm: 'https://fg-nrtm.ddns.net',
+    bg: 'http://bochil.ddns.net',
+    fgmods: 'https://api-fgmods.ddns.net'
+};
+
+global.APIKeys = {
+    'https://api.xteam.xyz': 'd90a9e986e18778b',
+    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+    'https://api.neoxr.my.id': 'yourkey',
+    'https://violetics.pw': 'beta',
+    'https://zenzapis.xyz': 'yourkey',
+    'https://api-fgmods.ddns.net': 'fg-dylux'
+};
+
+module.exports = {
+    WARN_COUNT: 3,
+    APIs: global.APIs,
+    APIKeys: global.APIKeys
+};
