8d19cdcdf54da89bc68dae2d3348238ebce052df
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-19 15:02:24 -0800

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,7 @@
 const settings = require("../settings");
 async function aliveCommand(sock, chatId, message) {
     try {
-        const message1 = `*🤖 Knight Bot is Active!*\n\n` +
+        const message1 = `*🤖 WALLYJAYTECH-MD is Active! 🤖*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
