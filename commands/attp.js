95b18826a08df8951cd210ef9c25bfcac94202ca
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-20 01:12:43 -0800

    Update attp.js

--- commands/attp.js
+++ commands/attp.js
@@ -14,7 +14,7 @@ async function attpCommand(sock, chatId, message) {
 
     try {
         const mp4Buffer = await renderBlinkingVideoWithFfmpeg(text);
-        const webpPath = await writeExifVid(mp4Buffer, { packname: 'Knight Bot' });
+        const webpPath = await writeExifVid(mp4Buffer, { packname: '🟨WALLY JAY TECH🟨' });
         const webpBuffer = fs.readFileSync(webpPath);
         try { fs.unlinkSync(webpPath) } catch (_) {}
         await sock.sendMessage(chatId, { sticker: webpBuffer }, { quoted: message });
