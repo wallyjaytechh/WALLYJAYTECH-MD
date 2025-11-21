479693e171993fb818ad34f3e22ef0aa62973c72
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-21 08:21:38 -0800

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,6 +1,6 @@
 async function unmuteCommand(sock, chatId) {
     await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: 'The group has been unmuted.' });
+    await sock.sendMessage(chatId, { text: '*🔴This group has been successfully unmuted🔴.*' });
 }
 
 module.exports = unmuteCommand;
