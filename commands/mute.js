523cfd8bb5708111461da771127520fad8834575
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-21 08:13:26 -0800

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -5,12 +5,12 @@ async function muteCommand(sock, chatId, senderId, message, durationInMinutes) {
 
     const { isSenderAdmin, isBotAdmin } = await isAdmin(sock, chatId, senderId);
     if (!isBotAdmin) {
-        await sock.sendMessage(chatId, { text: 'Please make the bot an admin first.' }, { quoted: message });
+        await sock.sendMessage(chatId, { text: '*Please make the bot an admin first.*' }, { quoted: message });
         return;
     }
 
     if (!isSenderAdmin) {
-        await sock.sendMessage(chatId, { text: 'Only group admins can use the mute command.' }, { quoted: message });
+        await sock.sendMessage(chatId, { text: '*🔸Only group admins can use the mute command🔸.*' }, { quoted: message });
         return;
     }
 
@@ -20,23 +20,23 @@ async function muteCommand(sock, chatId, senderId, message, durationInMinutes) {
         
         if (durationInMinutes !== undefined && durationInMinutes > 0) {
             const durationInMilliseconds = durationInMinutes * 60 * 1000;
-            await sock.sendMessage(chatId, { text: `The group has been muted for ${durationInMinutes} minutes.` }, { quoted: message });
+            await sock.sendMessage(chatId, { text: `*🔸The group has been muted for ${durationInMinutes} minutes.🔸*` }, { quoted: message });
             
             // Set timeout to unmute after duration
             setTimeout(async () => {
                 try {
                     await sock.groupSettingUpdate(chatId, 'not_announcement');
-                    await sock.sendMessage(chatId, { text: 'The group has been unmuted.' });
+                    await sock.sendMessage(chatId, { text: '*🔸This group has been unmuted successfully.🔸*' });
                 } catch (unmuteError) {
                     console.error('Error unmuting group:', unmuteError);
                 }
             }, durationInMilliseconds);
         } else {
-            await sock.sendMessage(chatId, { text: 'The group has been muted.' }, { quoted: message });
+            await sock.sendMessage(chatId, { text: '*🔸This group has been unmuted successfully.🔸*' }, { quoted: message });
         }
     } catch (error) {
         console.error('Error muting/unmuting the group:', error);
-        await sock.sendMessage(chatId, { text: 'An error occurred while muting/unmuting the group. Please try again.' }, { quoted: message });
+        await sock.sendMessage(chatId, { text: '*🥹An error occurred while muting/unmuting the group. Please try again.🥹*' }, { quoted: message });
     }
 }
 
