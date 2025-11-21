b2ca11fba8630600c96701e8ec000e6739c4f526
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-21 13:29:46 -0800

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,6 +1,6 @@
 async function clearCommand(sock, chatId) {
     try {
-        const message = await sock.sendMessage(chatId, { text: 'Clearing bot messages...' });
+        const message = await sock.sendMessage(chatId, { text: '*Clearing bot messages...*' });
         const messageKey = message.key; // Get the key of the message the bot just sent
         
         // Now delete the bot's message
