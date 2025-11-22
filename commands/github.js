e770e6b071535816b14bad65fbe5d3c3dd5b1156
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-22 02:02:07 -0800

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,19 +6,19 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/mruniquehacker/Knightbot-md');
+    const res = await fetch('https://api.github.com/repos/wallyjaytechh/WALLYJAYTECH-MD');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*乂  Knight Bot MD  乂*\n\n`;
-    txt += `✩  *Name* : ${json.name}\n`;
-    txt += `✩  *Watchers* : ${json.watchers_count}\n`;
-    txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
-    txt += `✩  *Last Updated* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
-    txt += `✩  *URL* : ${json.html_url}\n`;
-    txt += `✩  *Forks* : ${json.forks_count}\n`;
-    txt += `✩  *Stars* : ${json.stargazers_count}\n\n`;
-    txt += `💥 *KnightBot MD*`;
+    let txt = `*🟡 WALLYJAYTECH-MD 🟡*\n\n`;
+    txt += `🔴  *Name* : ${json.name}\n`;
+    txt += `🟠  *Watchers* : ${json.watchers_count}\n`;
+    txt += `🟣  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
+    txt += `🟤  *Last Updated* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
+    txt += `🟢  *Url* : ${json.html_url}\n`;
+    txt += `🔵  *Forks* : ${json.forks_count}\n`;
+    txt += `🔴  *Stars* : ${json.stargazers_count}\n\n`;
+    txt += `🟨 *Copyright wallyjaytech 2025* 🟨`;
 
     // Use the local asset image
     const imgPath = path.join(__dirname, '../assets/bot_image.jpg');
@@ -26,7 +26,7 @@ async function githubCommand(sock, chatId, message) {
 
     await sock.sendMessage(chatId, { image: imgBuffer, caption: txt }, { quoted: message });
   } catch (error) {
-    await sock.sendMessage(chatId, { text: '❌ Error fetching repository information.' }, { quoted: message });
+    await sock.sendMessage(chatId, { text: '*❌ Error fetching repository information.*' }, { quoted: message });
   }
 }
 
