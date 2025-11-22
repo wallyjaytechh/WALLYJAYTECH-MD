c8215c8c2732b4c9292777cd7b80914d583cc521
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-22 12:52:50 -0800

    Update truth.js

--- commands/truth.js
+++ commands/truth.js
@@ -13,10 +13,10 @@ async function truthCommand(sock, chatId, message) {
         const truthMessage = json.result;
 
         // Send the truth message
-        await sock.sendMessage(chatId, { text: truthMessage }, { quoted: message });
+        await sock.sendMessage(chatId, { text: *truthMessage* }, { quoted: message });
     } catch (error) {
         console.error('Error in truth command:', error);
-        await sock.sendMessage(chatId, { text: '❌ Failed to get truth. Please try again later!' }, { quoted: message });
+        await sock.sendMessage(chatId, { text: '*❌ Failed to get truth. Please try again later!*' }, { quoted: message });
     }
 }
 
