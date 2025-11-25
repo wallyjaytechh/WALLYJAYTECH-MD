1ad263e52b932b7ba3a48ad14e5120d315aecf38
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-25 21:50:34 +0100

    Update remini.js

--- commands/remini.js
+++ commands/remini.js
@@ -75,7 +75,7 @@ async function reminiCommand(sock, chatId, message, args) {
                     // Send the enhanced image
                     await sock.sendMessage(chatId, {
                         image: imageResponse.data,
-                        caption: '✨ *Image enhanced successfully!*\n\n𝗘𝗡𝗛𝗔𝗡𝗖𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧'
+                        caption: '✨ *Image enhanced successfully!*\n\n*ENHANCED BY WALLYJAYTECH-MD*'
                     }, { quoted: message });
                 } else {
                     throw new Error('Failed to download enhanced image');
