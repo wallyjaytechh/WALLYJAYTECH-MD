e09634ed94b935601b13b57e2ef4dd65edddfc1e
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-11-26 16:23:38 +0100

    ✌𝓦𝓐𝓛𝓛𝓨𝓙𝓐𝓨𝓣𝓔𝓒𝓗-𝓜𝓓✌

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -1,4 +1,4 @@
-const { downloadMediaMessage } = require('@whiskeysockets/baileys');
+ const { downloadMediaMessage } = require('@whiskeysockets/baileys');
 const axios = require('axios');
 const sharp = require('sharp');
 
